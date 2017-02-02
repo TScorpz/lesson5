d31812ecd1e2136b607b5c444a4940adc94104a9
TScorpz <shamaner88@gmail.com>
2017-02-03 01:05:40 +0200

    Init commit
    Init commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+});
+var Calculate = (function() {
+  var sum = 0;
+
+  return {
+    plus: function() {
+      var myArguments = arguments[0] || 0;
+      sum += myArguments;
+      return this;
+    },
+    minus: function() {
+      var myArguments = arguments[0] || 0;
+      sum -= myArguments;
+      return this;
+    },
+    multiply: function() {
+      var myArguments = arguments[0] || 1;
+      sum *= myArguments;
+      return this;
+    },
+    divide: function() {
+      var myArguments = arguments[0] || 1;
+      if (myArguments != 0) {
+        sum /= myArguments;
+      }
+      return this;
+    },
+    print: function() {
+      console.log(sum);
+    }
+  };
+})();
